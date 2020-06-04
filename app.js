367e4b3ccc02102132a003ab6e581d99d5d77c99
Edpi1313 <66421715+Edpi1313@users.noreply.github.com>
2020-06-04 07:17:36 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,140 @@
+function registrar(){
+    var email = document.getElementById('email').value;
+    var contrasena = document.getElementById('contrasena').value;
+
+    firebase.auth().createUserWithEmailAndPassword(email, contrasena)
+    .then(function(){
+        verficar()
+    })
+    
+    .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
+        // ...
+      });
+}
+
+function ingreso(){
+    
+    var email2 = document.getElementById('email2').value;
+    var contrasena2 = document.getElementById('contrasena2').value;
+    
+    firebase.auth().signInWithEmailAndPassword(email2, contrasena2)
+    
+    .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
+        // ...
+      });
+}
+
+function observador(){
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            console.log('existe usuario activo')
+            aparece(user);
+          // User is signed in.
+          var displayName = user.displayName;
+          
+          var email = user.email;
+          
+          console.log('*****************');
+          console.log(user.emailVerified)
+          console.log('*****************');
+          
+          var emailVerified = user.emailVerified;
+          var photoURL = user.photoURL;
+          var isAnonymous = user.isAnonymous;
+          var uid = user.uid;
+          var providerData = user.providerData;
+          // ...
+        } else {
+          // User is signed out.
+          console.log('no existe usuario activo')
+		  contenido.innerHTML = `
+		  
+		  
+		  
+		  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="num1.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="num2.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="num3.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+		  
+		  
+		  
+		  
+		  
+		  `;
+          // ...
+        }
+      });
+}
+observador();
+
+function aparece(user){
+    var user = user;
+    var contenido = document.getElementById('contenido');
+    if(user.emailVerified){
+        contenido.innerHTML = `
+		<div "class=container" mt-5>
+        <div class="alert alert-success" role="alert">
+			<h4 class="alert-heading">Bienvenido! ${user.email}</h4>
+			<p>Muy bien, estas dentro de uno de los mejores sitios web. REALMETE ESPERO SEA DE DU AGRADO y pueda pasar :) longer so that you can see how spacing within an alert works with this kind of content.</p>
+			<hr>
+			<p class="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice and tidy.</p>
+		</div>
+		
+        <button onclick="cerrar()" class="btn btn-danger">Cerrar sesión</button> 
+        </div>
+		`;
+    } 
+}
+
+function cerrar(){
+    firebase.auth().signOut()
+    .then(function(){
+        console.log('Saliendo...')
+    })
+    .catch(function(error){
+        console.log(error)
+    })
+}
+
+function verficar(){
+    var user = firebase.auth().currentUser;  
+    user.sendEmailVerification().then(function() {
+      // Email sent.
+      console.log('Enviando correo...');
+    }).catch(function(error) {
+      // An error happened.
+      console.log(error);
+    }); 
+}
